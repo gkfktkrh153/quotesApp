d105cf795aecf721928d9bcf810c69520b78716c
gkfktkrh153 <gkfktkrh153@naver.com>
2023-03-02 09:29:35 +0900

    1단계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("== 명언 앱 ==");
+        String s;
+        do {
+            System.out.print("명언) ");
+            s = sc.nextLine();
+
+
+        }
+        while(!s.equals("종료"));
+
+
     }
 }
