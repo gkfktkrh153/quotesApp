04e961aafa6b2861eedec4a974b4f3b523541649
gkfktkrh153 <gkfktkrh153@naver.com>
2023-03-02 09:42:01 +0900

    2단계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,14 +7,37 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
         String s;
+        int num = 0;
+        Quotes quotes = new Quotes();
         do {
             System.out.print("명언) ");
             s = sc.nextLine();
 
+            if (s.equals("등록")) {
+                quotes.register();
+            }
 
         }
         while(!s.equals("종료"));
 
 
+    }
+}
+class Quotes{
+    private String name;
+    private String author;
+    private int num;
+    private final Scanner sc;
+    Quotes(){
+        sc = new Scanner(System.in);
+    }
+    public void register(){
+
+        System.out.print("명언 : ");
+        name = sc.nextLine();
+        System.out.print("작가 : ");
+        author = sc.nextLine();
+
+        System.out.println(num + "번 명언이 등록되었습니다.");
     }
 }
