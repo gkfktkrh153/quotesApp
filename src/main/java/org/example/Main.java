615c6eefad9fe991111dba9ffcb2f13f446c943f
gkfktkrh153 <gkfktkrh153@naver.com>
2023-03-02 13:05:48 +0900

    9단계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,67 +1,97 @@
 package org.example;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 
 public class Main {
 
 
-    public static void main(String[] args) {
-        HashMap<Integer, Quotes> quotesList = new HashMap<>();
+    public static void main(String[] args) throws JsonProcessingException {
+        List<Quotes> quotesList = new ArrayList<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+
+        try {
+            FileReader reader = new FileReader("C:/codeLion/project/quotesApp/quotesApp/quotes.json");
+            quotesList = objectMapper.readValue(reader, new TypeReference<List<Quotes>>() {
+            });
+            reader.close();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
         Scanner sc = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
         String s;
         int num = 1;
         App app = new App();
 
-        do {
+        while(true){
             System.out.print("명령) ");
             s = sc.nextLine();
 
             if (s.equals("등록")) {
-                Quotes quotes = app.register(num);
-                quotesList.put(num++, quotes);
+                Quotes quotes = app.register(num++);
+                quotesList.add(quotes);
             }
             else if(s.equals("목록")) {
-                quotesList.forEach((key, q) -> {
-                    System.out.println(key + " / " + q.getAuthor() + " / " + q.getName());
-                });
+                System.out.println(quotesList.stream().
+                        map(Quotes::toString).
+                        collect(Collectors.joining("\n")));
             }
             else if(s.contains("삭제?id=")){
                 String[] split = s.split("=");
                 int deleteId = Integer.parseInt(split[1]);
-                Quotes remove = quotesList.remove(deleteId);
-                if(remove == null){
+                try{
+                    Quotes remove = quotesList.get(deleteId - 1);
+                    quotesList.remove(remove);
+                }
+                catch (IndexOutOfBoundsException e)
+                {
                     System.out.println(deleteId + "번 명언은 존재하지 않습니다.");
                 }
             }
             else if(s.contains("수정?id=")){
                 String[] split = s.split("=");
                 int updateId = Integer.parseInt(split[1]);
-                Quotes update = quotesList.get(updateId);
-                if(update == null){
+                try{
+                    Quotes updateObject = quotesList.get(updateId - 1);
+                    app.update(updateObject);
+                }
+                catch (IndexOutOfBoundsException e)
+                {
                     System.out.println(updateId + "번 명언은 존재하지 않습니다.");
-                    continue;
                 }
 
-                System.out.println("명언(기존) : " + update.getName());
-                System.out.print("명언 : ");
-                update.setName(sc.nextLine());
-
-                System.out.println("작가(기존) : " + update.getAuthor());
-                System.out.print("작가 : ");
-                update.setAuthor(sc.nextLine());
-
-            }
+            } else if (s.equals("종료"))
+                break;
             else
                 System.out.println("잘못된 명령입니다.");
-
-
-
         }
-        while(!s.equals("종료"));
+        String s1 = objectMapper.writeValueAsString(quotesList);
+        System.out.println(s1);
+
+        try {
+            FileWriter file = new FileWriter("C:/codeLion/project/quotesApp/quotesApp/quotes.json");
+            file.write(s1);
+            file.flush();
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
 
     }
@@ -80,16 +110,45 @@ class App {
         String author = sc.nextLine();
 
         System.out.println(num + "번 명언이 등록되었습니다.");
-        return new Quotes(name, author);
+        return new Quotes(num, name, author);
     }
     public void list(){
 
     }
 
+    public void update(Quotes updateObject) {
+        System.out.println("명언(기존) : " + updateObject.getName());
+        System.out.print("명언 : ");
+        updateObject.setName(sc.nextLine());
+
+        System.out.println("작가(기존) : " + updateObject.getAuthor());
+        System.out.print("작가 : ");
+        updateObject.setAuthor(sc.nextLine());
+    }
 }
-class Quotes{
+class Quotes {
+    private int id;
+    private String name;
+    private String author;
+    Quotes(){
+
+    }
+
+    public Quotes(int id, String name, String author) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+    }
     public String getName() {
         return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public void setName(String name) {
@@ -104,11 +163,8 @@ class Quotes{
         this.author = author;
     }
 
-    private String name;
-    private String author;
-
-    public Quotes(String name, String author) {
-        this.name = name;
-        this.author = author;
+    @Override
+    public String toString() {
+        return id + " / " + name + " / " + author;
     }
 }
